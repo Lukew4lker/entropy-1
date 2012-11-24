733c820efffe998b968d7bbd1467a19f62ebc0bf
Lucus16 <lars.jellema@gmail.com>
2012-11-24 17:52:32 +0100

    Updated linux build script and bootloader
    Also added init.bin example and fixed a bug in initiate.dasm.

--- build/build_project.sh
+++ build/build_project.sh
@@ -9,3 +9,15 @@ echo Building Kernel
 echo -------------------
 mono Organic.exe ../src/Kernel/main.dasm ../bin/entropy.img --working-directory ../src/Kernel/ --little-endian
 echo -------------------
+echo Building Disk
+echo -------------------
+mono Organic.exe ../src/Kernel/disk_data.dasm ../bin/disk_data.tmp --little-endian
+echo -------------------
+echo Building Init.bin
+echo -------------------
+mono Organic.exe ../src/Kernel/init.bin.dasm ../bin/init.bin.tmp --little-endian
+echo -------------------
+echo Making disk
+echo -------------------
+..\bin\makedisk.py
+echo -------------------
